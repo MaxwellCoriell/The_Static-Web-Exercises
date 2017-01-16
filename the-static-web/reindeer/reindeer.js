08c867fa228bed70a6af99a7285cf0a003c2ff06
Maxwell Baldridge <coolhandmax@Maxwells-MacBook-Pro.local>
2017-01-16 04:10:07 -0600

    added reindeer exercise

---
+++ the-static-web/reindeer/reindeer.js
@@ -0,0 +1,16 @@
+var colors = ["Blue", "Red", "Orange", "Purple", "Hazel", "Aquamarine", "Periwinkle", "Azure", "Fuchsia", "Chocolate", "Amber", "Amaranth"];
+var reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"];
+var fullNames = [];
+
+var hohohoElement = document.getElementById("coloredReindeer");
+
+var toHTML = "";
+
+for (var i = 0; i < reindeer.length; i++) {
+	var aReindeer = reindeer[i];
+	var aColor = colors[i];
+
+	toHTML += "<p>" + aColor + " " + aReindeer + "</p>";
+}
+
+hohohoElement.innerHTML = toHTML;
